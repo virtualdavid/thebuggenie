389be7e504e7afe0f5818c6cb4e2f18bd057c4ae
David Matson <githubdavid@gmail.com>
2014-05-17 11:59:26 -0700

    Fix double mention of main folder

--- modules/mailing/templates/_issueupdate.html.inc.php
+++ modules/mailing/templates/_issueupdate.html.inc.php
@@ -7,7 +7,6 @@
 	<div style="color: #888;">
 	--
 	<br>
-	Show issue: <?php echo link_tag("%thebuggenie_url%".make_url('viewissue', array('project_key' => $issue->getProject()->getKey(), 'issue_no' => $issue->getFormattedIssueNo()))); ?><br>
-	Show <?php echo $issue->getProject()->getName(); ?> project dashboard: <?php echo link_tag("%thebuggenie_url%".make_url('project_dashboard', array('project_key' => $issue->getProject()->getKey()))); ?>
-	</div>
+	Show issue: <?php echo link_tag("http://helpgivethanks.org".make_url('viewissue', array('project_key' => $issue->getProject()->getKey(), 'issue_no' => $issue->getFormattedIssueNo()))); ?><br>
+	Show <?php echo $issue->getProject()->getName(); ?> project dashboard: <?php echo link_tag("http://helpgivethanks.org".make_url('project_dashboard', array('project_key' => $issue->getProject()->getKey()))); ?>	</div>
 </div>
