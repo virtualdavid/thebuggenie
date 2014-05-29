fbc9636fda94d4b19e454687c148f6ebcace2167
David Matson <githubdavid@gmail.com>
2014-05-28 17:42:20 -0700

    Credit Sharron Denice for YouTube code, and take the blame for the rest

--- modules/main/templates/about.html.php
+++ modules/main/templates/about.html.php
@@ -15,7 +15,8 @@
 		
 		
 		<br>
-		<b>NOTE: 33 files have been changed in The Bug Genie v3.2.7.1 release and 2 added to accomplish and explain a template change entitled nitrogen.</b>
+		<b>NOTE: 33 files have been changed in The Bug Genie v3.2.7.1 release and 2 added to accomplish and explain a template change entitled nitrogen, mostly by David Matson.  Thanks to Sharron Denice for code copied from TBG’s master branch to enable showing YouTube videos in text areas: https://github.com/thebuggenie/thebuggenie/commit/8005f8721b4fceaf725f7f36c61b077986251c7a
+</b>
 		<br>
 		To compare CURRENT differences between the nitrogen template remix of TBG files and the original TBG v3.2.7.1 files, please visit <a target="_blank" href="https://github.com/virtualdavid/thebuggenie/compare/thebuggenie:branch-32...nitrogen?expand=1"> https://github.com/virtualdavid/thebuggenie/compare/thebuggenie:branch-32...nitrogen?expand=1 </a> <i>(opens in a new window)</i><br>
 		<br>
